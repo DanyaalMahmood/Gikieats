885351259b344b73152061ec05b333e89faf5175
Ahmad Maaz <ahmad.maaz273@gmail.com>
2023-04-09 00:20:06 +0500

    Ahmad added Cartscreen

--- client/screens/cart.js
+++ client/screens/cart.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, Switch } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, Switch ,Pressable} from 'react-native';
 import { Avatar } from 'react-native-elements';
 import { Ionicons } from '@expo/vector-icons';
 import myicon from '../assets/myicon.png';
+import burger from '../assets/burger.png';
+import salad from '../assets/salad.png';
+import pasta from '../assets/pasta.png';
 
-export default CartScreen = ({navigation}) => {
+export default CartScreen = ({ navigation }) => {
     return (
         <View style={styles.container}>
             <View style={styles.header}>
@@ -15,36 +18,114 @@ export default CartScreen = ({navigation}) => {
                 <View style={{ flex: 1 }} />
             </View>
             <Text style={styles.swipeText}>Swipe on an item to rotate</Text>
-            <View style={styles.itemContainer}>
-                <View style={styles.avatarContainer}>
-                    <Avatar
-                        rounded
-                        size={80}
-                        source={myicon}
-                        containerStyle={styles.avatar}
-                    />
-                    <View style={styles.textContainer}>
-                        <Text style={styles.avatarText}>My Picture</Text>
-                        <View style={styles.yellowContainer}>
-                            <Text style={styles.yellowText}>#1990</Text>
+            <View style={styles.maincontainer}>
+                <View style={styles.itemContainer}>
+                    <View style={styles.avatarContainer}>
+                        <Avatar
+                            rounded
+                            size={80}
+                            source={burger}
+                            containerStyle={styles.avatar}
+                        />
+                        <View style={styles.textContainer}>
+                            <Text style={styles.avatarText}>HamBurger</Text>
+                            <View style={styles.yellowContainer}>
+                                <Text style={styles.yellowText}>$500</Text>
+                            </View>
                         </View>
                     </View>
+                    <View style={styles.switchContainer}>
+                        <TouchableOpacity style={styles.switchButton}>
+                            <Text style={styles.switchText}>-</Text>
+                        </TouchableOpacity>
+                        <Text style={styles.quantityText}>1</Text>
+                        <TouchableOpacity style={styles.switchButton}>
+                            <Text style={styles.switchText}>+</Text>
+                        </TouchableOpacity>
+                    </View>
                 </View>
-                <View style={styles.switchContainer}>
-                    <TouchableOpacity style={styles.switchButton}>
-                        <Text style={styles.switchText}>-</Text>
-                    </TouchableOpacity>
-                    <Text style={styles.quantityText}>1</Text>
-                    <TouchableOpacity style={styles.switchButton}>
-                        <Text style={styles.switchText}>+</Text>
-                    </TouchableOpacity>
+                <View style={styles.itemContainer}>
+                    <View style={styles.avatarContainer}>
+                        <Avatar
+                            rounded
+                            size={80}
+                            source={salad}
+                            containerStyle={styles.avatar}
+                        />
+                        <View style={styles.textContainer}>
+                            <Text style={styles.avatarText}>Fresh Salad</Text>
+                            <View style={styles.yellowContainer}>
+                                <Text style={styles.yellowText}>$250</Text>
+                            </View>
+                        </View>
+                    </View>
+                    <View style={styles.switchContainer}>
+                        <TouchableOpacity style={styles.switchButton}>
+                            <Text style={styles.switchText}>-</Text>
+                        </TouchableOpacity>
+                        <Text style={styles.quantityText}>1</Text>
+                        <TouchableOpacity style={styles.switchButton}>
+                            <Text style={styles.switchText}>+</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+                <View style={styles.itemContainer}>
+                    <View style={styles.avatarContainer}>
+                        <Avatar
+                            rounded
+                            size={80}
+                            source={pasta}
+                            containerStyle={styles.avatar}
+                        />
+                        <View style={styles.textContainer}>
+                            <Text style={styles.avatarText}>Chicken Pasta</Text>
+                            <View style={styles.yellowContainer}>
+                                <Text style={styles.yellowText}>$899</Text>
+                            </View>
+                        </View>
+                    </View>
+                    <View style={styles.switchContainer}>
+                        <TouchableOpacity style={styles.switchButton}>
+                            <Text style={styles.switchText}>-</Text>
+                        </TouchableOpacity>
+                        <Text style={styles.quantityText}>1</Text>
+                        <TouchableOpacity style={styles.switchButton}>
+                            <Text style={styles.switchText}>+</Text>
+                        </TouchableOpacity>
+                    </View>
                 </View>
             </View>
+            <Pressable style={styles.button} onPress={() => navigation.navigate('VC')}>
+
+                <Text style={styles.buttonText}>
+                    Order Now
+                </Text>
+            </Pressable>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
+    button: {
+        backgroundColor: '#EFB60E',
+        borderRadius: 30,
+        width: 320,
+        height: 75,
+        alignItems: 'center',
+        justifyContent: 'center',
+        position: 'absolute',
+        bottom: 50,
+        alignSelf: 'center',
+      },
+      buttonText: {
+        color: 'white',
+        fontSize: 20,
+        fontWeight: '500'
+      },
+    maincontainer: {
+        top: 100,
+        color: 'white',
+    },
     container: {
         flex: 1,
         backgroundColor: '#F5F5F8',
@@ -54,7 +135,7 @@ const styles = StyleSheet.create({
         color: '#F5F5F8',
         flexDirection: 'row',
         alignItems: 'center',
-        alignContent:'space-between',
+        alignContent: 'space-between',
         paddingHorizontal: 16,
         paddingVertical: 10,
         // borderBottomWidth: 1,
@@ -75,13 +156,13 @@ const styles = StyleSheet.create({
         alignSelf: 'center'
     },
     itemContainer: {
-        top: 100,
+        // top: 100,
         flexDirection: 'row',
         alignItems: 'center',
         backgroundColor: '#FFF',
         padding: 16,
         margin: 16,
-        width: 300,
+        width: 350,
         alignSelf: 'center',
         borderRadius: 8,
         shadowColor: '#000',
@@ -89,6 +170,7 @@ const styles = StyleSheet.create({
         shadowOpacity: 0.2,
         shadowRadius: 2,
         elevation: 2,
+        borderRadius:30,
     },
     avatarContainer: {
         flexDirection: 'row',
