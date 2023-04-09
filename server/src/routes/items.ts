db912bccfee12af931fc9fbc2bac71f1e137cf7e
Tafreed Ahmad <107949435+TafreedAhmad@users.noreply.github.com>
2023-04-09 08:07:58 +0500

    added auth middleware

--- server/src/routes/items.ts
+++ server/src/routes/items.ts
@@ -2,12 +2,13 @@ import express from 'express';
 import { findDesi } from '../controllers/items.controller';
 import { findFastFood } from '../controllers/items.controller';
 import { findOthers } from '../controllers/items.controller';
+import { checkUser } from '../middlewares/auth.middleware';
 const itemRouter = express.Router();
 
 
 
-itemRouter.get('/desi', findDesi)
-itemRouter.get('/fastfood', findFastFood)
-itemRouter.get('/other', findOthers)
+itemRouter.get('/desi',checkUser, findDesi)
+itemRouter.get('/fastfood',checkUser, findFastFood)
+itemRouter.get('/other',checkUser, findOthers)
 
 export default itemRouter;
