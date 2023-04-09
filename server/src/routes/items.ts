2387405c4c89f4100b51ca391832f5f379e545a1
Tafreed Ahmad <107949435+TafreedAhmad@users.noreply.github.com>
2023-04-10 02:38:01 +0500

    added params with the routes where it was needed

--- server/src/routes/items.ts
+++ server/src/routes/items.ts
@@ -3,12 +3,18 @@ import { findDesi } from '../controllers/items.controller';
 import { findFastFood } from '../controllers/items.controller';
 import { findOthers } from '../controllers/items.controller';
 import { checkUser } from '../middlewares/auth.middleware';
+import { findSpecificItem } from '../controllers/items.controller';
+import { updateItem } from '../controllers/items.controller';
+import { vendorItems } from '../controllers/items.controller';
 const itemRouter = express.Router();
 
 
 
-itemRouter.get('/desi/:id',checkUser, findDesi)
-itemRouter.get('/fastfood/:id',checkUser, findFastFood)
-itemRouter.get('/other/:id',checkUser, findOthers)
+itemRouter.get('/desi/:id', checkUser, findDesi)
+itemRouter.get('/fastfood/:id', checkUser, findFastFood)
+itemRouter.get('/other/:id', checkUser, findOthers)
+itemRouter.get('/:itemId', checkUser, findSpecificItem)
+itemRouter.post('/:itemId', checkUser, updateItem)
+itemRouter.get('/', checkUser, vendorItems)
 
 export default itemRouter;
