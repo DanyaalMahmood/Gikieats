df04f0bfb0dfa067ad6afae2ada37b237cd1d3f2
Tafreed Ahmad <107949435+TafreedAhmad@users.noreply.github.com>
2023-04-10 04:18:55 +0500

    added the params to get the vendor id and checked availability before sending the data (#10)
    * added auth middleware
    
    * updated schema(do check it before merging, try making an erd diagram)
    
    * schema changes and creating order along with order item table
    
    * getting all the vendors
    
    * getting all the vendors
    
    * signup signin for vendors
    
    * added the vendor id in the routes as params
    
    * added params with the routes where it was needed
    
    ---------
    
    Co-authored-by: DanyaalMahmood <110989303+DanyaalMahmood@users.noreply.github.com>

--- server/src/controllers/items.controller.ts
+++ server/src/controllers/items.controller.ts
@@ -1,14 +1,54 @@
 import { Request, Response } from "express"
 import { PrismaClient, Prisma } from '@prisma/client'
+import jwt from 'jsonwebtoken';
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
+
+const secret = process.env.SECRET;
 
 
-const findDesi = async(req: Request, res: Response) => {
+const getVendor = async (req: Request, res: Response) => {
+  try {
+
+    const token = req.cookies.jwt;
+
+    if (token === null || typeof (secret) !== "string") {
+      res.status(400);
+      res.json({ "error": "jwt error" });
+      return false
+    }
+
+    let decode: any;
+
+    jwt.verify(token, secret, async (err: any, decodedToken: any) => {
+      if (err) {
+        res.json({ "error": "Not a valid json token" });
+        return false;
+      } else {
+        decode = jwt.decode(token);
+      }
+    });
+
+    if (decode === undefined || decode.regno === undefined) return;
+    const phoneno = decode.phoneno;
+
+    return phoneno;
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ error: 'An error occured while signing in!' });
+  }
+};
+
+const findDesi = async (req: Request, res: Response) => {
+  const vendorid = String(req.params.id);
   try {
     const desiItems = await prisma.item.findMany({
       where: {
-        category: 'desi'
+        AND: [
+          { category: 'desi' },
+          { vendorid: vendorid },
+          { availability: 'true' }
+        ]
       }
     })
     res.status(200).json(desiItems)
@@ -18,11 +58,17 @@ const findDesi = async(req: Request, res: Response) => {
 }
 
 
-const findFastFood = async(req: Request, res: Response) => {
+
+const findFastFood = async (req: Request, res: Response) => {
+  const vendorid = String(req.params.id);
   try {
     const fastfoodItems = await prisma.item.findMany({
       where: {
-        category: 'fastfood'
+        AND: [
+          { category: 'fastfood' },
+          { vendorid: vendorid },
+          { availability: 'true' }
+        ]
       }
     })
     res.status(200).json(fastfoodItems)
@@ -32,11 +78,16 @@ const findFastFood = async(req: Request, res: Response) => {
 }
 
 
-const findOthers = async(req: Request, res: Response) => {
+const findOthers = async (req: Request, res: Response) => {
+  const vendorid = String(req.params.id);
   try {
     const otherItems = await prisma.item.findMany({
       where: {
-        category: 'other'
+        AND: [
+          { category: 'desi' },
+          { vendorid: vendorid },
+          { availability: 'true' }
+        ]
       }
     })
     res.status(200).json(otherItems)
@@ -45,4 +96,107 @@ const findOthers = async(req: Request, res: Response) => {
   }
 }
 
-export {findDesi, findFastFood, findOthers};
+const findSpecificItem = async (req: Request, res: Response) => {
+  const itemId = String(req.params.itemId);
+
+  try {
+    const item = await prisma.item.findUnique({
+      where: {
+        id: itemId,
+      },
+    });
+
+    if (item) {
+      res.status(200).json(item);
+    } else {
+      res.status(404).json({ error: 'Item not found' });
+    }
+  } catch (err) {
+    res.status(500).json({ error: 'Something went wrong' });
+  }
+}
+
+const updateItem = async (req: Request, res: Response) => {
+  try {
+    const phoneno = await getVendor(req, res);
+    const vendor = await prisma.vendor.findFirst({
+      where: {
+        phoneno: phoneno
+      },
+    });
+
+    if (!vendor) {
+      return res.status(404).json({ error: 'Vendor not found' });
+    }
+
+
+    const vendorId = vendor.id;
+    const itemId = req.params.itemId;
+    const { name, description, image, price, availability, category } = req.body;
+
+    const existingItem = await prisma.item.findFirst({
+      where: {
+        id: itemId,
+        vendorid: vendorId
+      },
+    });
+
+
+    if (!existingItem) {
+      return res.status(404).json({ error: 'Item not found or vendor not authorized' });
+    }
+
+    const updatedItem = await prisma.item.update({
+      where: {
+        id: itemId,
+      },
+      data: {
+        name: name,
+        description: description,
+        image: image,
+        price: parseInt(price),
+        availability: availability,
+        category: category
+      }
+    });
+
+    res.status(200).json(updatedItem);
+
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ error: 'Something went wrong' });
+  }
+}
+
+const vendorItems = async (req: Request, res: Response) => {
+
+  try {
+    const phoneno = await getVendor(req, res);
+    const vendor = await prisma.vendor.findFirst({
+      where: {
+        phoneno: phoneno
+      },
+    });
+
+    if (!vendor) {
+      return res.status(404).json({ error: 'Vendor not found' });
+    }
+
+    const vendorId = vendor.id;
+
+    const items = await prisma.item.findMany({
+      where: {
+        vendorid: vendorId
+      }
+    });
+
+    res.status(200).json(items);
+
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ error: 'Something went wrong' });
+  }
+}
+
+
+export { findDesi, findFastFood, findOthers, findSpecificItem, updateItem, vendorItems };
